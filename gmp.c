c45b34172feaf42d779468d6da403a2f365fd00a
Sajith Sasidharan <sajith@nonzen.in>
2015-10-22 13:57:53 -0400

    Add license header in gmp.c.

--- gmp.c
+++ gmp.c
@@ -1,3 +1,13 @@
+/*
+ *  Quick check for gmp.
+ *
+ *  Compile with: '$CC -Wall gmp.c -o gmp -lgmp'
+ *  
+ *  Copying and distribution of this file, with or without
+ *  modification, are permitted in any medium without royalty provided
+ *  the copyright notice and this notice are preserved.  This file is
+ *  offered as-is, without any warranty.
+ */
 
 #include <stdio.h>
 #include <gmp.h>
